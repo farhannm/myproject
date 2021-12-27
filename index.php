428b3b6ff483b996438dffd355b1ef494bce91d7
Farhan Maulana <96566356+farhannm@users.noreply.github.com>
2021-12-27 10:30:59 +0700

    index,php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+/*for ($i=1; $i < 1000; $i++) {
+    echo "Aku sayang kamu <br>";
+}*/
+
+/*$i=0;
+while ($i < 10){
+    echo "PENGEN UANG! <br>"; 
+$i++;
+} */
+
+echo date("l-d-M-Y", mktime(0,0,0,4,21,2005));
+
+?>
